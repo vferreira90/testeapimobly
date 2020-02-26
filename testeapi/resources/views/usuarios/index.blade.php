387946afddb8ce6df46db614a9aa2f0c89ab00d4
vferreira90 <46329440+vferreira90@users.noreply.github.com>
2020-02-26 13:21:09 -0300

    Update index.blade.php

--- testeapi/resources/views/usuarios/index.blade.php
+++ testeapi/resources/views/usuarios/index.blade.php
@@ -1,40 +1,73 @@
-<ul class="nav-bar">
-  <li class="nav-item">
-    <a class="nav-link" href="{{ route('usuarios.index') }}">Listar Usuarios</a>
-    <a class="nav-link" href="{{ route('usuarios.create') }}">Criar Usuarios</a>
-  </li>
-</ul>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<div class="container">
-  <div class="col">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>index usuarios</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <table class="table">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Comandos</th>
-        </tr>
-      </thead>
-      <tbody>
+</head>
 
-        @foreach($usuarios as $usuario)
+<body>
 
-        <tr>
-          <th scope="row">{{$usuario['id']}}</h3>
-          </th>
-          <td>{{$usuario['name']}}</td>
-          <td>
-            <a href="{{ route('usuarios.show', ['usuario' => $usuario['id']]) }}">detalhes</a>
-            <a href="{{ route('usuarios.edit', ['usuario' => $usuario['id']]) }}">Editar</a>
-            <a href="#">Apagar</a>
-            <a href="{{ route('rota.posts', ['id' => $usuario['id']]) }}">posts</a>
-          </td>
-        </tr>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Teste Mobly API</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{ route('usuarios.index') }}">Usuarios <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('usuarios.create') }}">Criar Usuarios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Listar Posts</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-        @endforeach
+  <div class="container">
+    <div class="col">
 
-      </tbody>
-    </table>
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Comandos</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          @foreach($usuarios as $usuario)
+
+          <tr>
+            <th scope="row">{{$usuario['id']}}</h3>
+            </th>
+            <td>{{$usuario['name']}}</td>
+            <td>
+              <a href="{{ route('usuarios.show', ['usuario' => $usuario['id']]) }}">Detalhes</a>
+              <a href="{{ route('usuarios.edit', ['usuario' => $usuario['id']]) }}">Editar</a>
+              <a href="#">Apagar</a>
+              <a href="{{ route('rota.posts', ['id' => $usuario['id']]) }}">Posts</a>
+            </td>
+          </tr>
+
+          @endforeach
+
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
